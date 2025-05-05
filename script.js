f03402e89926aaa2597e1bad939015920fc5fb6c
islom709 <rislom709@gmail.com>
2025-05-05 23:28:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var islom1 = prompt("ismingizni kiriting")
+var islom2= prompt("familayningizni kiriting")
+var islom3 = prompt("Yoshingiz kiriting")
+var islom4 = prompt("yoqtirgan sohanigiz yozing")
+var islom5 = prompt( "yashash tumanigiz")
+var islom6 = prompt("jonkamisiz")
+var islom7 = alert("Malumotlarinigizni kiritganiz uchun thank you I LOVE YOU")
+console.log(islom1 );
+console.log( islom2 );
+console.log( islom3 );
+console.log( islom4 );
+console.log( islom5 );
+console.log( islom6 );
+
+
